a62bf6da002a0d23af9da35c18f966a3c1148188
T R A D E R <142751437+hamitrader@users.noreply.github.com>
2023-10-09 00:43:04 +0500

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,14 @@
+import os,platform
+print(f'\x1b[1;92m[√] PLEASE WAIT CHECKING UPDATE...')
+os.system('git pull')
+os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+os.system("clear")
+print('\033[1;32m [•] JOIN ONLY TRADING WhATSAP GROUP')
+os.system('xdg-open https://chat.whatsapp.com/GsR7eXGFlrjIfQiBcc8dc7')
+os.system("clear")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Marshal import HAMI
+    HAMI()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
